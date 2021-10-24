eb25c1afadfeaa3350ba2365a515c911747cd468
thesotog <62193559+thesotog@users.noreply.github.com>
2021-10-24 14:48:33 -0400

    Create webmin-install.sh

---
+++ webmin-install.sh
@@ -0,0 +1,3 @@
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.981_all.deb
+apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip
+dpkg --install webmin_1.981_all.deb
