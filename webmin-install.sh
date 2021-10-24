419e9566c84fc5af79cffe39984fba3f65529cf3
thesotog <62193559+thesotog@users.noreply.github.com>
2021-10-24 15:59:26 -0400

    Update webmin-install.sh

--- webmin-install.sh
+++ webmin-install.sh
@@ -1,3 +1,4 @@
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.981_all.deb
+apt update -y && apt upgrade -y
 apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip
 dpkg --install webmin_1.981_all.deb
