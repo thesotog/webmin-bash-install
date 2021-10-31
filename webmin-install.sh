627a46225c2dce0887ba9e22a508bf093fe3a7b7
soto <62193559+thesotog@users.noreply.github.com>
2021-10-31 20:26:16 +0000

    Update webmin-install.sh

--- webmin-install.sh
+++ webmin-install.sh
@@ -1,3 +1,5 @@
+apt update -y && apt upgrade -y
+apt install lamp-server^ -y
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.981_all.deb
 apt update -y && apt upgrade -y
 apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip
